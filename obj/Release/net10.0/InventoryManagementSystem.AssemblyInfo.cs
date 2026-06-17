9657daed9c3142037ab523d9a835264eb190e064
Rukhsar8889 <rnasir15@gmail.com>
2026-06-17 20:46:28 +0500

    New Push with vercel json

--- obj/Release/net10.0/InventoryManagementSystem.AssemblyInfo.cs
+++ obj/Release/net10.0/InventoryManagementSystem.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("InventoryManagementSystem")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4483b27fae0bbef455b9dcb9b3819a662a95cb5c")]
 [assembly: System.Reflection.AssemblyProductAttribute("InventoryManagementSystem")]
